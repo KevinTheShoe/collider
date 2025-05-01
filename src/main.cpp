4574b44bdf0ae16e956d9d7def5c055aa3673017
Kevin <kevinshu.colorado@gmail.com>
2025-05-01 14:28:12 -0600

    Completed Methods

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,16 @@
 #include <iostream>
 #include <vector>
 #include <array>
+#include <complex>
 
 using namespace std;
 
 // === Translation of GLSL vector types. Additionally expanded to double type ===
+
 struct vec2 {
 	double x;
 	double y;
-	// constructors
+	// Constructors
 	vec2(double x_in, double y_in) : x(x_in), y(y_in) {}
 	vec2(double both_in) : x(both_in), y(both_in) {}
 	// Vector and Vector operations
@@ -42,7 +44,7 @@ struct vec3 {
 	double x;
 	double y;
 	double z;
-	// constructors
+	// Constructors
 	vec3(double x_in, double y_in, double z_in) : x(x_in), y(y_in), z(z_in) {}
 	vec3(double both_in) : x(both_in), y(both_in), z(both_in) {}
 	// Vector and number operations
@@ -55,6 +57,12 @@ struct vec3 {
 	vec3 operator/(double num) const {
 		return vec3(x/num, y/num, z/num);
 	}
+};
+struct bezier {
+	vec2 A;
+	vec2 B;
+	vec2 C;
+	bezier(vec2 A_in, vec2 B_in, vec2 C_in) : A(A_in), B(B_in), C(C_in) {}
 };
 
 // === Translation of GLSL Functions ===
@@ -95,7 +103,7 @@ double testCross(vec2 a, vec2 b, vec2 p) {
 }
 
 // Determine which side we're on (using barycentric parameterization)
-double signBezier(vec2 A, vec2 B, vec2 C, vec2 p) {
+double signBezier(vec2 p, vec2 A, vec2 B, vec2 C) {
 	vec2 a = C - A, b = B - A, c = p - A;
 	vec2 bary = vec2(c.x*b.y-b.x*c.y, a.x*c.y-c.x*a.y) / (a.x*b.y-b.x*a.y);
 	vec2 d = vec2(bary.y * 0.5, 0.0) + 1.0 - bary.x - bary.y;
@@ -129,10 +137,13 @@ vec3 solveCubic(double a, double b, double c) {
 }
 
 // Find the signed distance from a point to a bezier curve
-double sdBezier(vec2 p, vec2 A, vec2 B, vec2 C) {
-	B = mix(B + vec2(1e-4), B, step(1e-6, abs(B * 2.0 - A - C)));
+double bezierSDF(vec2 p, bezier in) {
+	vec2 A = in.A;
+	vec2 B = in.B;
+	vec2 C = in.C;
+	B = mix(B + vec2(1e-4), B, step(1e-6, abs(B * 2.0 - A - C))); // correction for catastrophic cancellation
 	vec2 a = B - A, b = A - B * 2.0 + C, c = a * 2.0, d = A - p;
-	vec3 k = vec3(3.*dot(a,b),2.*dot(a,a)+dot(d,b),dot(d,a)) / dot(b,b);
+	vec3 k = vec3(3.0*dot(a,b), 2.0*dot(a,a) + dot(d,b), dot(d,a)) / dot(b,b);
 	vec3 t = clamp(solveCubic(k.x, k.y, k.z), 0.0, 1.0);
 	vec2 pos = A + (c + b*t.x)*t.x;
 	double dis = length(pos - p);
@@ -140,171 +151,122 @@ double sdBezier(vec2 p, vec2 A, vec2 B, vec2 C) {
 	dis = min(dis, length(pos - p));
 	pos = A + (c + b*t.z)*t.z;
 	dis = min(dis, length(pos - p));
-	return dis * signBezier(A, B, C, p);
+	return dis * signBezier(p, A, B, C);
 }
 
-float cos_acos_3(float x) {
-	x = sqrt(0.5+0.5*x);
-	return x*(x*(x*(x*-0.008972+0.039071)-0.107074)+0.576975)+0.5;
-}
-
-double bezier_sdf(double px, double py, double Ax, double Ay, double Bx, double By, double Cx, double Cy) {
-	double ax = Bx - Ax;
-	double ay = By - Ay;
-	double bx = Ax - 2.0*Bx + Cx;
-	double by = Ay - 2.0*By + Cy;
-	double cx = 2.0*ax;
-	double cy = 2.0*ay;
-	double dx = Ax - px;
-	double dy = Ay - py;
-
-	double kk = 1.0 / (bx * bx + by * by);
-	double kx = kk * (ax * bx + ay * by);
-	double ky = kk * (2.0 * (ax * ax + ay * ay) + (dx * bx + dy * by)) / 3.0;
-	double kz = kk * (dx * ax + dy * ay);
-
-	double res = 0.0;
-	double sgn = 0.0;
-
-	double p  = ky - kx * kx;
-	double q  = kx * (2.0 * kx * kx - 3.0 * ky) + kz;
-	double p3 = p * p * p;
-	double q2 = q * q;
-	double h  = q2 + 4.0 * p3;
-
-	if (h >= 0.0) {
-		h = sqrt(h);
-		double xx = (h - q) / 2.0;
-		double xy = (-h -q) / 2.0;
-
-		#if 0
-		// When p≈0 and p<0, h-q has catastrophic cancelation. So, we do
-		// h=√(q²+4p³)=q·√(1+4p³/q²)=q·√(1+w) instead. Now we approximate
-		// √ by a linear Taylor expansion into h≈q(1+½w) so that the q's
-		// cancel each other in h-q. Expanding and simplifying further we
-		// get x=vec2(p³/q,-p³/q-q). And using a second degree Taylor
-		// expansion instead: x=vec2(k,-k-q) with k=(1-p³/q²)·p³/q
-		if(abs(p) < 0.001) {
-			float k = p3 / q;              // linear approx
-			//float k = (1.0-p3/q2)*p3/q;  // quadratic approx
-			x = vec2(k, -k - q);
-		}
-		#endif
-
-		double xxsign = (xx < 0) ? -1 : 1;
-		double xysign = (xy < 0) ? -1 : 1;
-
-		double uvx = xxsign * cbrt(abs(xx));
-		double uvy = xysign * cbrt(abs(xy));
-
-		double t = uvx + uvy;
-
-		// from NinjaKoala - single newton iteration to account for cancellation
-		t -= (t*(t*t+3.0*p)+q)/(3.0*t*t+3.0*p);
-
-		t = clamp(t - kx, 0.0, 1.0);
-		double wx = dx + (cx + bx * t) * t;
-		double wy = dy + (cy + by * t) * t;
-		double outQx = wx + px;
-		double outQy = wy + py;
-
-		res = wx * wx + wy * wy;
-
-		double tempx = (cx + 2.0 * bx * t);
-		double tempy = (cy + 2.0 * by * t);
-		sgn = tempx * wy - tempy * wx;
-	}
-	else
-	{   // 3 roots
-		double z = sqrt(-p);
-		#if 0
-		double v = acos(q/(p*z*2.0))/3.0;
-		double m = cos(v);
-		double n = sin(v);
-		#else
-		double m = cos_acos_3(q/(p*z*2.0));
-		double n = sqrt(1.0-m*m);
-		#endif
-		n *= sqrt(3.0);
-
-		double tx = clamp((m+m)*z-kx, 0.0, 1.0);
-		double ty = clamp((-n-m)*z-kx, 0.0, 1.0);
-		double tz = clamp((n-m)*z-kx, 0.0, 1.0);
-
-		double qxx = dx+(cx+bx*tx)*tx;
-		double qxy = dy+(cy+by*tx)*tx;
-		double d2x = qxx * qxx + qxy * qxy;
-		double tempx = (ax+bx*tx);
-		double tempy = (ay+by*tx);
-		double sx = tempx*qxy-tempy*qxx;
-
-		double qyx = dx+(cx+bx*ty)*ty;
-		double qyy = dy+(cy+by*ty)*ty;
-		double d2y = qyx * qyx + qyy * qyy;
-		double temp2x = (ax+bx*ty);
-		double temp2y = (ay+by*ty);
-		double sy = temp2x*qyy-temp2y*qyx;
-
-		if (d2x < d2y) {
-			res = d2x;
-			sgn = sx;
-			double outQx = qxx + px;
-			double outQy = qxy + py;
-		} else {
-			res = d2y;
-			sgn = sy;
-			double outQx = qyx + px;
-			double outQy = qyy + py;
-		}
-	}
-
-	return sqrt(res) * ((sgn < 0) ? -1 : 1);
-}
-
-double bezier_deriv(double t, double A, double B, double C) {
-	return 2 * (1 - t) * (B - A) + 2 * t * (C - B);
-}
-
-double bezier_deriv2(double t, double A, double B, double C) {
-	return 2*(A - 2*B + C);
-}
-
-array<double, 2> collision_beziers(double Ax1, double Ay1, double Bx1, double By1, double Cx1, double Cy1, double Ax2, double Ay2, double Bx2, double By2, double Cx2, double Cy2, int method, int n, double starting_estimate) {
-	// Ax1: x coordinate of P0 in bezier 1, Cy2: y coordinate of P2 in bezier 2, etc.
+vec2 findAnalyticIntersection(bezier b1, bezier b2, int method, int n, double estimate0, double estimate1, double estimate2) {
+	// Analytically find point on bezier A that causes SDF of bezier B to equal 0
 	//
 	// Methods:
 	// 0 = secant method
+	// 1 = bisection method
+	// 2 = muller's method
 	//
-	// n: number of iterations to perform. 0 adjusts until accurate, or a limit of 100
+	// n: number of iterations to perform. 0 adjusts until accurate to 0.1, or a limit of 100
 	//
-	// Returns point of collision as an array<double,2>{x, y}
+	// Returns point of collision as a vec2
 
 	// converting bezier to normal form: at^2 + bt + c
 	// (A - 2B + C)t^2 + (2B - 2A)t + A
-	// function we are trying to find root of is bezier_sdf((ax*t^2 + bx*t + cx), (ay*t^2 + by*t + cy), Ax2, Ay2, Bx2, By2, Cx2, Cy2)
-	double ax = Ax1 - 2*Bx1 + Cx1;
-	double ay = Ay1 - 2*By1 + Cy1;
-	double bx = 2*Bx1 - 2*Ax1;
-	double by = 2*By1 - 2*Ay1;
-	double cx = Ax1;
-	double cy = Ay1;
+	vec2 a = b1.A - b1.B*2.0 + b1.C;
+	vec2 b = (b1.B - b1.A)*2.0;
+	vec2 c = b1.A;
 
 	if (n == 0) n = 100;
 
-	// secant method
-	double t0 = 0;
-	double t1 = starting_estimate;
-	double t = 0;
-	for (int i = 0; i < n; i++) {
-		double f_t1 = bezier_sdf((ax*t1*t1 + bx*t1 + cx), (ay*t1*t1 + by*t1 + cy), Ax2, Ay2, Bx2, By2, Cx2, Cy2);
-		if (abs(f_t1) <= 0.1) break;
-		double f_t0 = bezier_sdf((ax*t0*t0 + bx*t0 + cx), (ay*t0*t0 + by*t0 + cy), Ax2, Ay2, Bx2, By2, Cx2, Cy2);
-		t = t1 - f_t1*((t1 - t0)/(f_t1 - f_t0));
-		t0 = t1;
-		t1 = t;
-		// cout << t << endl;
+	switch (method) {
+		case 0: {
+			// Secant method
+			double t0 = estimate0;
+			double t1 = estimate1;
+			double t = 0;
+			for (int i = 0; i < n; i++) {
+				double f_t1 = bezierSDF((a*t1*t1 + b*t1 + c), b2);
+				if (abs(f_t1) < 0.1) break;
+				double f_t0 = bezierSDF((a*t0*t0 + b*t0 + c), b2);
+				t = t1 - f_t1*((t1 - t0)/(f_t1 - f_t0));
+				t0 = t1;
+				t1 = t;
+			}
+			return a*t*t + b*t + c;
+		}
+		case 1: {
+			// Bisection method
+			// We must find t0, t1 such that f(t0) is negative and f(t1) is positive.
+			double t0 = estimate0;
+			double t1 = 1;
+			double t = 0;
+			double step = 2;
+			double f_t1 = bezierSDF((a*t1*t1 + b*t1 + c), b2);
+			for (int i = 0; i < pow(2, 5); i++) {
+				double f_t0 = bezierSDF((a*t0*t0 + b*t0 + c), b2);
+				if (f_t0 * f_t1 <= 0) break;
+				if (t0 + step >= 1.0) {
+					step /= 2.0;
+					t0 = step / 2.0;
+				}
+				else t0 += step;
+			}
+			// Actual method
+			for (int i = 0; i < n; i++) {
+				t = (t0 + t1)/2;
+				if (bezierSDF((a*t*t + b*t + c), b2) * bezierSDF((a*t0*t0 + b*t0 + c), b2) > 0) t0 = t;
+				else t1 = t;
+			}
+			return a*t*t + b*t + c;
+		}
+		case 2: {
+			// Muller's method
+			double t0 = estimate0;
+			double t1 = estimate1;
+			double t2 = estimate2;
+			for (int i = 0; i < n; i++) {
+				double h0 = t1 - t0;
+				double h1 = t2 - t1;
+				double f_t0 = bezierSDF((a*t0*t0 + b*t0 + c), b2);
+				double f_t1 = bezierSDF((a*t1*t1 + b*t1 + c), b2);
+				double f_t2 = bezierSDF((a*t2*t2 + b*t2 + c), b2);
+				double d0 = (f_t1 - f_t0)/h0;
+				double d1 = (f_t2 - f_t1)/h1;
+				double a_m = (d1 - d0)/(h1 + h0);
+				double b_m = a_m*h1 + d1;
+				double c_m = f_t2;
+				double t3 = t2 - (2*c_m)/max(
+					abs(b_m + pow(pow(b_m, 2) - 4*a_m*c_m, 1/2)),
+					abs(b_m - pow(pow(b_m, 2) - 4*a_m*c_m, 1/2))
+				);
+				t0 = t1;
+				t1 = t2;
+				t2 = t3;
+			}
+			return a*t2*t2 + b*t2 + c;
+		}
+		default: {
+			cout << "Illegal Method" << endl;
+			return vec2(0, 0);
+		}
 	}
-	return array<double, 2>{ax*t*t + bx*t + cx, ay*t*t + by*t + cy};
+}
+
+vector<vec2> findAllAnalyticIntersection(bezier b1, bezier b2, int method, int n) {
+	// Results only include those with error less than 0.1
+	// Points of intersection are considered separate if separated by distance of at least 1
+	vector<vec2> results;
+	int sections = 10;
+	for (int i = 0; i <= sections; i++) {
+		vec2 result = findAnalyticIntersection(b1, b2, method, n, i*(1.0/sections), 0.5, 1.0);
+		int isIn = 0;
+		for (vec2 r : results) {
+			if (abs(r.x - result.x) < 1 && abs(r.y - result.y) < 1) {
+				isIn = 1;
+				break;
+			}
+		}
+		if (isIn == 0 && abs(bezierSDF(result, b1)) < 0.1 && abs(bezierSDF(result, b2)) < 0.1) {
+			results.push_back(result);
+		}
+	}
+	return results;
 }
 
 int main(void) {
@@ -313,84 +275,112 @@ int main(void) {
 	// SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI | FLAG_WINDOW_RESIZABLE);
 	SetConfigFlags(FLAG_WINDOW_HIGHDPI | FLAG_WINDOW_RESIZABLE);
 	InitWindow(1280, 720, "Collider");
-	SetTargetFPS(60);
+	SetTargetFPS(6000);
 
-	Vector2 points1[3];
-	points1[0] = Vector2{0, 0};
-	points1[1] = Vector2{800, 0};
-	points1[2] = Vector2{800, 450};
-	Vector2 points2[3];
-	points2[0] = Vector2{800, 0};
-	points2[1] = Vector2{0, 0};
-	points2[2] = Vector2{0, 450};
+	vector<vector<bezier>> shapes;
+	shapes.push_back({bezier(vec2(20, 20), vec2(GetScreenWidth() - 220, 20), vec2(GetScreenWidth() - 220, GetScreenHeight() - 20))});
+	shapes.push_back({bezier(vec2(GetScreenWidth() - 220, 20), vec2(20, 20), vec2(20, GetScreenHeight() - 20))});
 
-	Vector2* adjusting = nullptr;
+	vec2* adjusting = nullptr;
 
 	int n = 3;
-	double starting_estimate = 0.5;
+	double p[3] = {0.0, 0.5, 1.0};
+	int changing_p = 0;
+	int method = 0;
+
+	const float ctrlpt_size = 10;
 
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
 
-		for (int i = 0; i < 3; i++) {
-			DrawRectangle(points1[i].x - 5, points1[i].y - 5, 10, 10, ORANGE);
-			if (IsMouseButtonUp(MOUSE_BUTTON_LEFT)) adjusting = nullptr;
-			else if (adjusting == nullptr && abs(points1[i].x - GetMousePosition().x) < 5 && abs(points1[i].y - GetMousePosition().y) < 5) adjusting = &points1[i];
+		// Handle all shapes
+		for (int i = 0; i < shapes.size(); i++) {
+			for (int i2 = 0; i2 < shapes[i].size(); i2++) {
+				bezier b = shapes[i][i2];
+				// Draw control points
+				DrawRectangle(b.A.x - ctrlpt_size/2, b.A.y - ctrlpt_size/2, ctrlpt_size, ctrlpt_size, ORANGE);
+				DrawRectangle(b.B.x - ctrlpt_size/2, b.B.y - ctrlpt_size/2, ctrlpt_size, ctrlpt_size, ORANGE);
+				DrawRectangle(b.C.x - ctrlpt_size/2, b.C.y - ctrlpt_size/2, ctrlpt_size, ctrlpt_size, ORANGE);
+				// Handle adjustment
+				if (IsMouseButtonUp(MOUSE_BUTTON_LEFT)) adjusting = nullptr;
+				else if (adjusting == nullptr && abs(b.A.x - GetMousePosition().x) < 5 && abs(b.A.y - GetMousePosition().y) < 5) adjusting = &shapes[i][i2].A;
+				else if (adjusting == nullptr && abs(b.B.x - GetMousePosition().x) < 5 && abs(b.B.y - GetMousePosition().y) < 5) adjusting = &shapes[i][i2].B;
+				else if (adjusting == nullptr && abs(b.C.x - GetMousePosition().x) < 5 && abs(b.C.y - GetMousePosition().y) < 5) adjusting = &shapes[i][i2].C;
+				// Draw contact points with all other shapes
+				for (int i3 = i + 1; i3 < shapes.size(); i3++) {
+					for (bezier b2 : shapes[i3]) {
+						// for (vec2 p : findAllAnalyticIntersection(b, b2, 1, n)) {
+						// 	DrawCircle(p.x, p.y, 5, GREEN);
+						// }
+						vec2 pt = findAnalyticIntersection(b, b2, method, n, p[0], p[1], p[2]);
+						cout << pt.x << endl;
+						DrawCircle(pt.x, pt.y, 5, GREEN);
+					}
+				}
+				// Draw the bezier
+				Vector2 points[3] = {
+					Vector2{static_cast<float>(b.A.x), static_cast<float>(b.A.y)},
+					Vector2{static_cast<float>(b.B.x), static_cast<float>(b.B.y)},
+					Vector2{static_cast<float>(b.C.x), static_cast<float>(b.C.y)}
+				};
+				DrawSplineBezierQuadratic(points, 3, 1, RED);
+			}
 		}
-		for (int i = 0; i < 3; i++) {
-			DrawRectangle(points2[i].x - 5, points2[i].y - 5, 10, 10, ORANGE);
-			if (IsMouseButtonUp(MOUSE_BUTTON_LEFT)) adjusting = nullptr;
-			else if (adjusting == nullptr && abs(points2[i].x - GetMousePosition().x) < 5 && abs(points2[i].y - GetMousePosition().y) < 5) adjusting = &points2[i];
-		}
+
 		if (adjusting != nullptr) {
-			(*adjusting).x = GetMousePosition().x;
-			(*adjusting).y = GetMousePosition().y;
+			adjusting->x = GetMousePosition().x;
+			adjusting->y = GetMousePosition().y;
 		}
+		if (IsKeyPressed(KEY_ONE)) changing_p = 0;
+		if (IsKeyPressed(KEY_TWO)) changing_p = 1;
+		if (IsKeyPressed(KEY_THREE)) changing_p = 2;
 		if (IsKeyPressed(KEY_UP)) n++;
 		if (IsKeyPressed(KEY_DOWN) && n > 0) n--;
-		if (IsKeyPressed(KEY_RIGHT) && starting_estimate < 0.99) starting_estimate += 0.1;
-		if (IsKeyPressed(KEY_LEFT) && starting_estimate > 0.01) starting_estimate -= 0.1;
+		if (IsKeyPressed(KEY_RIGHT) && p[changing_p] < 0.99) p[changing_p] += 0.1;
+		if (IsKeyPressed(KEY_LEFT) && p[changing_p] > 0.01) p[changing_p] -= 0.1;
 
-		DrawSplineBezierQuadratic(points1, 3, 1, RED);
-		DrawSplineBezierQuadratic(points2, 3, 1, BLUE);
-
-		array<double, 2> collision_point = collision_beziers(
-			points1[0].x, points1[0].y, points1[1].x, points1[1].y, points1[2].x, points1[2].y,
-			points2[0].x, points2[0].y, points2[1].x, points2[1].y, points2[2].x, points2[2].y,
-			0, n, starting_estimate);
-
-		double ax = points1[0].x - 2*points1[1].x + points1[2].x;
-		double ay = points1[0].y - 2*points1[1].y + points1[2].y;
-		double bx = 2*points1[1].x - 2*points1[0].x;
-		double by = 2*points1[1].y - 2*points1[0].y;
-		double cx = points1[0].x;
-		double cy = points1[0].y;
-		DrawCircle(collision_point[0], collision_point[1], 5, GREEN);
-
-		// === Draw UI ===
+		// === Handle UI ===
 		DrawRectangle(GetScreenWidth() - 200, 0, 200, GetScreenHeight(), LIGHTGRAY);
 		// Secant Button
-		DrawRectangle(GetScreenWidth() - 175, 25, 150, 45, GRAY);
+		Rectangle secant_button = Rectangle{static_cast<float>(GetScreenWidth() - 175), 25, 150, 45};
+		if (method == 0) DrawRectangleRec(secant_button, DARKGRAY);
+		else DrawRectangleRec(secant_button, GRAY);
 		DrawText("Secant", GetScreenWidth() - 165, 35, 25, BLACK);
+		if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(GetMousePosition(), secant_button)) {
+			method = 0;
+		}
 		// Bisection Button
-		DrawRectangle(GetScreenWidth() - 175, 95, 150, 45, GRAY);
+		Rectangle bisection_button = Rectangle{static_cast<float>(GetScreenWidth() - 175), 95, 150, 45};
+		if (method == 1) DrawRectangleRec(bisection_button, DARKGRAY);
+		else DrawRectangleRec(bisection_button, GRAY);
 		DrawText("Bisection", GetScreenWidth() - 165, 105, 25, BLACK);
+		if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(GetMousePosition(), bisection_button)) {
+			method = 1;
+		}
 		// Muller Button
-		DrawRectangle(GetScreenWidth() - 175, 165, 150, 45, GRAY);
+		Rectangle muller_button = Rectangle{static_cast<float>(GetScreenWidth() - 175), 165, 150, 45};
+		if (method == 2) DrawRectangleRec(muller_button, DARKGRAY);
+		else DrawRectangleRec(muller_button, GRAY);
 		DrawText("Muller", GetScreenWidth() - 165, 175, 25, BLACK);
+		if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(GetMousePosition(), muller_button)) {
+			method = 2;
+		}
 		// n
 		DrawText(TextFormat("n: %i", n), GetScreenWidth() - 175, 235, 25, BLACK);
-		// p0
-		DrawText(TextFormat("p0: %f", starting_estimate), GetScreenWidth() - 175, 285, 25, BLACK);
+		// p
+		DrawText(TextFormat("p0: %f", p[0]), GetScreenWidth() - 175, 285, 25, changing_p == 0 ? DARKGRAY : BLACK);
+		DrawText(TextFormat("p1: %f", p[1]), GetScreenWidth() - 175, 335, 25, changing_p == 1 ? DARKGRAY : BLACK);
+		DrawText(TextFormat("p2: %f", p[2]), GetScreenWidth() - 175, 385, 25, changing_p == 2 ? DARKGRAY : BLACK);
 		// Instructions
+		DrawText("1,2,3: select p", GetScreenWidth() - 190, GetScreenHeight() - 75, 20, BLACK);
 		DrawText("U/D: change n", GetScreenWidth() - 190, GetScreenHeight() - 50, 20, BLACK);
-		DrawText("L/R: change p0", GetScreenWidth() - 190, GetScreenHeight() - 25, 20, BLACK);
+		DrawText("L/R: change p", GetScreenWidth() - 190, GetScreenHeight() - 25, 20, BLACK);
 
-		// DrawText(TextFormat("%f", bezier_sdf(GetMousePosition().x, GetMousePosition().y, points1[0].x, points1[0].y, points1[1].x, points1[1].y, points1[2].x, points1[2].y)), 25, GetScreenHeight() - 75, 20, LIGHTGRAY);
-		DrawText(TextFormat("%f", sdBezier(vec2(GetMousePosition().x, GetMousePosition().y), vec2(points1[0].x, points1[0].y), vec2(points1[1].x, points1[1].y), vec2(points1[2].x, points1[2].y))), 25, GetScreenHeight() - 75, 20, LIGHTGRAY);
-		DrawText(TextFormat("%f %f", collision_point[0], collision_point[1]), 25, GetScreenHeight() - 50, 20, LIGHTGRAY);
-		DrawText(TextFormat("%i", n), 25, GetScreenHeight() - 25, 20, LIGHTGRAY);
+		DrawText(TextFormat("%f", bezierSDF(vec2(GetMousePosition().x, GetMousePosition().y), shapes[0][0])), 25, GetScreenHeight() - 75, 20, LIGHTGRAY);
+		// DrawText(TextFormat("%f", bezierSDF(vec2(GetMousePosition().x, GetMousePosition().y), vec2(points1[0].x, points1[0].y), vec2(points1[1].x, points1[1].y), vec2(points1[2].x, points1[2].y))), 25, GetScreenHeight() - 75, 20, LIGHTGRAY);
+		// DrawText(TextFormat("%f %f", collision_point[0], collision_point[1]), 25, GetScreenHeight() - 50, 20, LIGHTGRAY);
+		// DrawText(TextFormat("%i", n), 25, GetScreenHeight() - 25, 20, LIGHTGRAY);
 
 		DrawFPS(GetScreenHeight() - 25, GetScreenHeight() - 25);
 
